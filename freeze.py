a5a249623c965c56afec4d78e651b0aa6132238f
difonz <kevindifonzo@hotmail.com>
2025-06-12 18:13:52 -0400

    Updating for netlify

--- freeze.py
+++ freeze.py
@@ -1,12 +1,8 @@
 from flask_frozen import Freezer
 from app import app
-import os
-import shutil
 
 
 freezer = Freezer(app)
 
 if __name__ == '__main__':
-    if os.path.isdir('build'):
-        shutil.rmtree('build')
     freezer.freeze()
